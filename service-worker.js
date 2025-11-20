e35a145f03c9e6f7c2e64ab63dd2fe8a5c72e818
Ryan Green <ryanmgreen96@gmail.com>
2025-11-20 08:54:11 -0800

    dd

--- service-worker.js
+++ service-worker.js
@@ -9,8 +9,11 @@ const FILES_TO_CACHE = [
   "/PDFviewerMain/icon.png",
   "/PDFviewerMain/fall24.html",
   "/PDFviewerMain/winter24.html",
-  "/PDFviewerMain/service-worker.js",
   "/PDFviewerMain/spring25.html",
+  "/PDFviewerMain/summer25.html",
+  "/PDFviewerMain/fall25.html",
+  "/PDFviewerMain/service-worker.js",
+  
 ];
 
 self.addEventListener("install", (event) => {
